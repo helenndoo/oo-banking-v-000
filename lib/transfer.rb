ed75e69a111209cb0bd8d74a7797e4ac1a2ca094
Helen Du <helendu.614@gmail.com>
2018-04-06 18:25:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,21 +16,21 @@ class Transfer
   end
 
   def execute_transaction
-    if self.status != "complete" && self.sender.balance > amount
-      self.sender.balance -= amount
-      self.receiver.balance = amount
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      if self.valid? && self.status == "pending" && sender.balance > amount
+        receiver.balance = amount
+        sender.balance -= amount
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
     end
-  end
 
-  def reverse_transfer
-    if self.status == "complete"
-      self.receiver.balance -= amount
-      self.sender.balance = amount
-      self. status = "reversed"
+    def reverse_transfer
+      if self.valid? && self.status == "complete" && sender.balance > amount
+        receiver.balance -= amount
+        sender.balance = amount
+        self.status = "reversed"
+      end
     end
-  end
 end
