ffb6dae3e9e0a1a9c88af6ccf80020b2395d97c9
Helen Du <helendu.614@gmail.com>
2018-04-06 18:23:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,36 +1,37 @@
-class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @status = "pending"
-    @amount = amount
-  end
-
-  def valid?
-    if self.sender.valid? && self.receiver.valid?
-      true
-    else false
-    end
-  end
-
-  def execute_transaction
-    if self.status != "complete" && self.sender.balance > amount
-      self.sender.balance -= amount
-      self.receiver.balance = amount
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
-  end
-
-  def reverse_transfer
-    if self.status == "complete"
-      self.receiver.balance -= amount
-      self.sender.balance = amount
-      self. status = "reversed"
-    end
-  end
-end
++require_relative "bank_account.rb"
++require 'pry'
++
+ class Transfer
+-  # your code here
++  attr_accessor :sender, :receiver, :status, :amount
++
++  def initialize(sender, receiver, amount)
++    @sender = sender
++    @receiver = receiver
++    @status = "pending"
++    @amount = amount
++  end
++
++  def valid?
++    @sender.valid? && @receiver.valid? && @sender.balance >= @amount
++  end
++
++  def execute_transaction
++    if self.valid? && @status == "pending"
++      @sender.balance -= @amount
++      @receiver.balance += @amount
++      @status = "complete"
++    else
++      @status = "rejected"
++      "Transaction rejected. Please check your account balance."
++    end
++  end
++
++  def reverse_transfer
++    if @status == "complete"
++      @sender.balance += @amount
++      @receiver.balance -= @amount
++      @status = "reversed"
++    end
++  end
+ end
