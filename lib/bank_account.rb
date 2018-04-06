9f37ea004893941f65b26f084171c0e41bd1c437
Helen Du <helendu.614@gmail.com>
2018-04-06 18:22:30 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,26 +1,33 @@
 class BankAccount
-  attr_accessor :balance, :status
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
-  end
-
-  def deposit(amount)
-      @balance = @balance + amount
-    end
-
-  def display_balance
-    "Your balance is $#{balance}."
-  end
-
-  def valid?
-    @status == "open" && @balance> 0
-  end
-  def close_account
-    @status = "closed"
-  end
++  attr_accessor :name, :balance, :status
++
++  def initialize(name)
++    @name = name.freeze
++    @balance = 1000
++    @status = "open"
++  end
++
++  def name=(name)
++    raise NoMethodError if @name != name
++  end
++
++  def deposit(amount)
++    self.balance = self.balance + amount
++  end
++
++  def display_balance
++    "Your balance is $#{self.balance}."
++  end
++
++  def valid?
++    if self.balance > 0 && self.status == "open"
++      true
++    else false
++    end
++  end
++
++  def close_account
++    self.status = "closed"
++  end
 
 end
